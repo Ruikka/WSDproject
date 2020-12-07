5d42080c66fc188fc6ff106f3861527fcf020909
Anna Ruikka <ruikkaa1@aalto.fi>
2020-12-07 14:23:10 +0200

    landing page modded and some aesthetic changes

--- routes/controllers/rootController.js
+++ routes/controllers/rootController.js
@@ -2,17 +2,22 @@ import { getMood } from '../../services/summaryService.js'
 
 const getRoot = async({render, session}) => {
   const user = await session.get('user');
-  const user_id = user.id
+  if(user) {
+    const user_id = user.id
 
-  var d = new Date()
-  const today = await getMood(user_id, d.getFullYear(), d.getMonth()+1, d.getDate())
-  d.setDate(d.getDate() - 1);
-  const yesterday = await getMood(user_id, d.getFullYear(), d.getMonth()+1, d.getDate())
+    var d = new Date()
+    const today = await getMood(user_id, d.getFullYear(), d.getMonth()+1, d.getDate())
+    d.setDate(d.getDate() - 1);
+    const yesterday = await getMood(user_id, d.getFullYear(), d.getMonth()+1, d.getDate())
+  
+    const msg = await session.get('msg');
+    await session.set('msg', '')
+  
+    render('rootView.ejs', {msg: msg, average_mood_today: today, average_mood_yesterday: yesterday});
+  } else {
+    render('rootViewGuest.ejs');
+  }
 
-  const msg = await session.get('msg');
-  await session.set('msg', '')
-
-  render('rootView.ejs', {msg: msg, average_mood_today: today, average_mood_yesterday: yesterday});
 };
  
 export { getRoot };
