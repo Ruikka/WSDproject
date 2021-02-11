5c8b4dcd47a00da0b8ce966a2ede187bcdf8361e
Anna Ruikka <ruikkaa1@aalto.fi>
2021-02-11 12:49:01 +0200

    ye

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
-let config = { tls: {enforce: true}};
+let config = {};
 
 if (Deno.env.get('TEST_ENVIRONMENT')) {
     config.database = {};
     config.port = 7777
   } else {
-    const DATABASE_URL = Deno.env.toObject().DATABASE_URL+'?sslmode=require';
+    const DATABASE_URL = Deno.env.toObject().DATABASE_URL;
     const PORT = Number(Deno.env.toObject().PORT);
     console.log(`DB: ${DATABASE_URL}, PORT: ${PORT}`)
     config.database = DATABASE_URL 
