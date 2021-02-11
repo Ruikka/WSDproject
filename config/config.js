9a3a7f7bb636a8895c60e7607834c00a23143b0c
Anna Ruikka <ruikkaa1@aalto.fi>
2021-02-11 12:26:22 +0200

    SSL force on again

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ if (Deno.env.get('TEST_ENVIRONMENT')) {
     config.database = {};
     config.port = 7777
   } else {
-    const DATABASE_URL = Deno.env.toObject().DATABASE_URL;
+    const DATABASE_URL = Deno.env.toObject().DATABASE_URL+'?sslmode=require';
     const PORT = Number(Deno.env.toObject().PORT);
     console.log(`DB: ${DATABASE_URL}, PORT: ${PORT}`)
     config.database = DATABASE_URL 
